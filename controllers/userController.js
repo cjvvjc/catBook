0b2add15204270c0bf2151605558e073c02d27e4
Chad Volk <cjvolk@gmail.com>
2023-07-21 10:45:06 -0600

    added userController

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const passport = require('passport')
+const User = require('../models/userModel') //'..' up one folder; '.' in same folder
+
+//render login and register pages
+const loginPage = (req, res) => {
+  res.render('login')
+}
+
+const registerPage = (req, res) => {
+  res.render('register')
+}
+
+ //basic authentication function using local strategy. if login successful, go to home page. if unsuccessful, reload login page. 
+const loginUser = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: false
+})
+
+//registration operation
+const registerUser = async (req, res) => {
+  try {
+    const {username, password} = req.body //taking info from form and putting into db. destructuring username and password variables
+
+    const user = new User({username}) //create new instance of User model passing in user name
+    await User.register(user, password) //using passport register the new user
+    passport.authenticate('local')(req, res, function() { // automatically login user: authenticate user and redirect to home page
+      res.redirect('/')
+    })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/register')
+  }
+}
+
+//logout user
+const logoutUser = (req, res) => {
+  res.logout(function(err) {
+    if(err) {return next(err)}
+    res.redirect('/')
+  })
+}
+
+module.export = {
+  loginUser,
+  loginPage,
+  registerPage,
+  registerUser, 
+  logoutUser
+}
